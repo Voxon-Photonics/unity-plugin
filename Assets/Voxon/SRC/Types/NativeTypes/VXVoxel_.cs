14267e227f21e59aeb37b0896257640ddcbc646c
voxon-ben <ben@voxon.co>
2020-03-05 09:02:05 +1030

    VXVoxel fix. holding off replacing vxvoxel until impact assessment

---
+++ Assets/Voxon/SRC/Types/NativeTypes/VXVoxel_.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Voxon
+{
+    public class VXVoxel_ : IDrawable
+    {
+        VoxelView _voxelView;
+
+        public VXVoxel_(Vector3 pos, Color32 col) : this(pos, col, null)
+        {
+        }
+
+        public VXVoxel_(Vector3 pos, Color32 col, GameObject parent)
+        {
+            _voxelView = new VoxelView(pos, col, parent);
+            VXProcess.Drawables.Add(this);
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+            _voxelView.Draw();
+        }
+    }
+}
